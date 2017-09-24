5ae79dd5d65de82580a5f4daf937863c18a27d73
Andrew Jenkins <andrewjjenkins@gmail.com>
2017-09-23 19:35:29 -0600

    Add express-winston to log every request

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressWinston = require('express-winston');
 const log = require('./log');
 const router = require('./router');
 const inputs = require('./lib/inputs');
@@ -10,6 +11,10 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(expressWinston.logger({
+  winstonInstance: log,
+  colorize: true,
+}));
 
 var port = process.env.PORT || 8081;
 app.listen(port);
